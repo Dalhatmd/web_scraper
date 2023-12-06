2b65bf2220a89c70009d412aa9121fd95d94dd56
Dalhatmd <umarwalid4055@gmail.com>
2023-12-06 18:03:14 +0100

    Scraped prices, locations and number of beds and baths

--- scraper.py
+++ scraper.py
@@ -1,7 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
-r = requests.get("https://www.century21.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/")
+r = requests.get("https://www.century21.com/real-estate/atlanta-ga/LCGAATLANTA/")
 c = r.content
 soup = BeautifulSoup(c, "html.parser")
 
 all = soup.find_all("div", {"class" : "property-card-primary-info"})
+price = all[0].find("a", {"class" : "listing-price"}).text
+#print(all)
+#print(price.replace('\n', '').replace(' ',''))
+
+for item in all:
+    price = item.find("a", {"class" : "listing-price"}).text
+    address = item.find("div" , {"class" : "property-address"}).text
+    print(address.replace(' ','').replace('\n' , ''))
+    city = item.find("div" , {"class" : "property-city"}).text
+    print(city.replace('\n' , '').replace(' ', ''))
+    try:
+        beds = item.find("div" , {"class" : "property-beds"}).text
+        print(beds.replace('\n', '').replace(' ', ''))
+    except:
+        print("No bed information available")
+        pass
+    try:
+        baths = item.find("div" , {"class" : "property-baths"}).text
+        print(baths.replace('\n', '').replace(' ', ''))
+    except:
+        print("No bath information available")
+        pass
+    print(price.replace('\n', '').replace(' ',''))
+    print()
